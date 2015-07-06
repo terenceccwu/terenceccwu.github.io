e47229a0410e8e3319e1a7ce28655484367b4bd2
terenceccwu <terenceccwu@gmail.com>
2015-07-06 17:28:03 +0800

    added image frame to index.html

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 var count = 0;
 function changecontent() {
-	if(count == 0) {
-		document.getElementById("image_frame").innerHTML = "<center><h3>1. you have changed the content!</h3></center>";
-		count++;
-	}else{
-		document.getElementById("image_frame").innerHTML = "<center><h3>2. this is a div tag</h3></center>";
-		count--;
-	}
+		document.getElementById("image_frame").innerHTML = "<center><img width=300px height=300px src=\"./images/"+ count +".jpg\"></center>";
+		count = (count + 1) % 6;
 }
 
 document.getElementById("image_frame").addEventListener("click", changecontent);
