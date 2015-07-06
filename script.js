3c0ea83f072143b590e3243a67c0b6f6f4794e18
terenceccwu <terenceccwu@gmail.com>
2015-07-06 16:23:11 +0800

    removed button and fixed typo

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var count = 0;
 function changecontent() {
 	if(count == 0) {
-		document.getElementById("image_frame").innerHTML = "<center><h3>you have changed the countent!</h3></center>";
+		document.getElementById("image_frame").innerHTML = "<center><h3>1. you have changed the content!</h3></center>";
 		count++;
 	}else{
-		document.getElementById("image_frame").innerHTML = "<center><h3>this is a div tag</h3></center>";
+		document.getElementById("image_frame").innerHTML = "<center><h3>2. this is a div tag</h3></center>";
 		count--;
 	}
 }
