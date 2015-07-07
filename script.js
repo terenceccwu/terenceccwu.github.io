12102fe362b93e7e3eaf7ded5120c8e71f7d65a9
terenceccwu <terenceccwu@gmail.com>
2015-07-07 12:40:51 +0800

    added preload image module, updated image DOM selector method

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+//preload images
+var no_of_image = 6;
+var images = new Array()
+
+for(var i = 0; i < 6 ; i++)
+{
+	images[i] = new Image()
+	images[i].src = "./images/" + i.toString() + ".jpg";
+	console.log(images[i]);
+}
+
+
+
 var count = 0;
 function changecontent() {
-		document.getElementById("image_frame").innerHTML = "<center><img width=300px height=300px src=\"./images/"+ count +".jpg\"></center>";
+		if(count == 0) {
+			document.getElementById("image_frame").innerHTML = "<center><img id='the_image' width=300px height=300px src=\"./images/"+ 0 +".jpg\"></center>";
+			
+		} else {
+			var x = document.getElementById("the_image");
+			x.setAttribute("src","./images/" + count + ".jpg");
+		}
+
 		count = (count + 1) % 6;
 }
 
